ae4f74022f23571e9fdafda7018966e85f943cdd
gdghanu-Nhu13 <gdschanu.nhuttq1306@gmail.com>
2025-04-01 20:08:12 +0700

    java-core [Synchronous and Asynchronous]

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,32 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //syncFuntionExample();
+        asyncFuntionExample();
+    }
+    static void asyncFuntionExample() {
         System.out.println("Bắt đầu");
-        task();
+
+        Thread thread = new Thread(() -> doTaskAsync());
+
+        thread.start();
+
         System.out.println("Kết thúc");
     }
-
-    static void task() {
+    static void doTaskAsync() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Tác vụ đã hoàn thành");
+    }
+    static void syncFuntionExample(){
+        System.out.println("Bắt đầu");
+        doTask();
+        System.out.println("Kết thúc");
+    }
+    static void doTask() {
         try {
             Thread.sleep(2000);
             System.out.println("Task đã hoàn thành");
