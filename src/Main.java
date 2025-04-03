71343c4d455d6da69896e083edecf41cd6562719
gdghanu-Nhu13 <gdschanu.nhuttq1306@gmail.com>
2025-04-03 21:11:42 +0700

    java core [collection]

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+/*
+import java.util.*;
+
+ class ListExample {
     public static void main(String[] args) {
-        //syncFuntionExample();
-        asyncFuntionExample();
-    }
-    static void asyncFuntionExample() {
-        System.out.println("Bắt đầu");
+        List<String> names = new ArrayList<>();
+        names.add("Nhu");
+        names.add("Linh");
+        names.add("Anh");
+        names.add("Nhu"); // Trùng lặp vẫn được chấp nhận
 
-        Thread thread = new Thread(() -> doTaskAsync());
-
-        thread.start();
-
-        System.out.println("Kết thúc");
-    }
-    static void doTaskAsync() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Tác vụ đã hoàn thành");
-    }
-    static void syncFuntionExample(){
-        System.out.println("Bắt đầu");
-        doTask();
-        System.out.println("Kết thúc");
-    }
-    static void doTask() {
-        try {
-            Thread.sleep(2000);
-            System.out.println("Task đã hoàn thành");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        System.out.println("List: " + names);
     }
 }
+*/
+
+/*import java.util.*;
+
+class SetExample {
+    public static void main(String[] args) {
+        Set<String> uniqueNames = new HashSet<>();
+        uniqueNames.add("Nhu");
+        uniqueNames.add("Linh");
+        uniqueNames.add("Anh");
+        uniqueNames.add("Nhu"); // Bị loại bỏ vì đã tồn tại
+
+        System.out.println("Set: " + uniqueNames);
+    }
+}*/
+
+/*import java.util.*;
+
+class MapExample {
+    public static void main(String[] args) {
+        Map<Integer, String> students = new HashMap<>();
+        students.put(1, "Nhu");
+        students.put(2, "Linh");
+        students.put(3, "Anh");
+        students.put(1, "Minh"); // Ghi đè giá trị của key 1
+
+        System.out.println("Map: " + students);
+    }
+}*/
+
+
