a0c4c4b80f65c689dcce6bc5c08d3ca5700ea22f
gdghanu-Nhu13 <gdschanu.nhuttq1306@gmail.com>
2025-04-05 21:30:29 +0700

    java core [collection]

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ import java.util.*;
         names.add("Nhu"); // Trùng lặp vẫn được chấp nhận
 
         System.out.println("List: " + names);
+        // ở list muốn in riêng từng thứ tự các tên cũng có thể dùng cách này
+        names.get(0);
     }
 }
 */
@@ -25,7 +27,17 @@ class SetExample {
         uniqueNames.add("Nhu"); // Bị loại bỏ vì đã tồn tại
 
         System.out.println("Set: " + uniqueNames);
-    }
+
+        // ở set thường thì nó ko duy trì trật tự các thứ tự nên các ptu không có chỉ số, nên để truy cậvafavaof ptu của
+        set thì tường phải lọc qua toàn bộ set, v để làm đc thì dùng foreach
+      uniqueNames.forEach(name -> System.out.println(name));
+
+        uniqueNames.remove("Nhu"); xóa
+        System.out.println(uniqueNames.contains(Nhu)); ktra xem nhu co trong set không
+        System.out.printl(uniqueNames.size()); ktra xem độ lớn của set
+        System.out.println(uniqueNames.isEmpty()); ktra xem set có rỗng ko, nếu trả về true thì là rỗng
+
+nói chung dùng set khi mà muốn loại bỏ các phần tử trùng lawpj, và ko quan tâm thứ tự
 }*/
 
 /*import java.util.*;
