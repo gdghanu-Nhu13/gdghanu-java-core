b40c88bacfda70e7230500f0743a9e467cafdf55
gdghanu-Nhu13 <gdschanu.nhuttq1306@gmail.com>
2025-04-08 17:10:07 +0700

    java-core [enum]

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,30 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //syncFuntionExample();
-        asyncFuntionExample();
-    }
-    static void asyncFuntionExample() {
-        System.out.println("Bắt đầu");
-
-        Thread thread = new Thread(() -> doTaskAsync());
-
-        thread.start();
-
-        System.out.println("Kết thúc");
-    }
-    static void doTaskAsync() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Tác vụ đã hoàn thành");
-    }
-    static void syncFuntionExample(){
-        System.out.println("Bắt đầu");
-        doTask();
-        System.out.println("Kết thúc");
-    }
-    static void doTask() {
-        try {
-            Thread.sleep(2000);
-            System.out.println("Task đã hoàn thành");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+enum PizzaStatus {
+    order(5),
+    prepare(10),
+    delivery(15);
+    int time;
+    // mỗi gtri enum là 1 instance cuar enum đó
+    PizzaStatus(int time) {
+        this.time = time;
     }
 }
+
+class EnumExample {
+    public static void main(String[] args) {
+        PizzaStatus status = PizzaStatus.order;
+        if(status == PizzaStatus.order) {
+            System.out.println("Pizza has already ordered");
+        }
+        // neu muon in ra tat ca ptu
+        for(PizzaStatus s : PizzaStatus.values()) {
+            System.out.println(s);
+        }
+        // neu muon cac trang thai order co time nhat dinh co the dung field
+        System.out.println(PizzaStatus.delivery.time);
+        // khi tạo các trường dl cho enum cần dùng final đam bảo gtri các trường ko thay đổi
+
+    }
+}
+// nói chung enum là mục đich đảm bảo các gtri ko thay đổi
